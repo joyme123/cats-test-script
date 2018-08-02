752592987fe7e2d9f63b412e818a586bcfeb4412
joyme <jiangpengfei12@gmail.com>
2018-08-02 23:23:02 +0800

    更新测试例子

--- php/index.php
+++ php/index.php
@@ -6,4 +6,14 @@ $result['SERVER'] = $_SERVER;
 $result['GET'] = $_GET;
 $result['POST'] = $_POST;
 
+foreach($_FILES as $key => $file) {
+    if (!move_uploaded_file($file['tmp_name'], "./".$file['name'])) {
+        echo "文件移动失败";
+    }
+
+    $_FILES[$key]['filepath'] = $file['name'];
+}
+
+$result['FILE'] = $_FILES;
+
 echo json_encode($result);
